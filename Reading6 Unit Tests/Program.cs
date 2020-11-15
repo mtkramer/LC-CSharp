0d746f550790a695cba859405782af1e24082ed9
mtkramer <themtkramer@gmail.com>
2020-11-15 03:02:08 -0500

    Reading6 init

---
+++ Reading6 Unit Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reading6_Unit_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota", "Prius", 10, 50);
+            Console.WriteLine(car.Make + " - " + car.Model);
+        }
+    }
+}

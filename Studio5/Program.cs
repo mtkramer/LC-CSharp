8526bf5334a104a8b09919fcb5c1a8331a8987b6
mtkramer <themtkramer@gmail.com>
2020-11-15 02:43:35 -0500

    Studio5 bonus complete

--- Studio5/Program.cs
+++ Studio5/Program.cs
@@ -9,12 +9,10 @@ namespace Studio5
             // DateTime now = DateTime.Now;
             // Console.WriteLine($"Ticks: {now.Ticks}");
             Menu menu = new Menu();
-            /*
             MenuItem steak = new MenuItem("Seasoned, seared, and served the way you want.", 18, 1);
             menu.AddMenuItem("Steak", steak);
             MenuItem wings = new MenuItem("Hot, sweet, and chewy.", 9, 0);
             menu.AddMenuItem("Buffalo Wings", wings);
-            */
             MenuItem brownie = new MenuItem("With chocolate chips and a scoop of vanilla ice cream.", 9, 2);
             menu.AddMenuItem("Chocolate Brownie", brownie);
             menu.AddMenuItem("Chocolate Brownie", brownie);

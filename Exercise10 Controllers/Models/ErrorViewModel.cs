a16ff8c2bc84167a1f1e727970cd91cd615d4b4e
mtkramer <themtkramer@gmail.com>
2020-11-20 15:35:02 -0500

    Exercise10 init

---
+++ Exercise10 Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exercise10_Controllers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

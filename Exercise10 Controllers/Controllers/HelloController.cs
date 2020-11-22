24fe92b4de3bd78b1601b1b93532c312cd5429d5
mtkramer <themtkramer@gmail.com>
2020-11-22 14:38:37 -0500

    Reading10 complete

--- Exercise10 Controllers/Controllers/HelloController.cs
+++ Exercise10 Controllers/Controllers/HelloController.cs
@@ -6,19 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exercise10_Controllers.Controllers
 {
+    [Route("/helloworld")]
     public class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
-        [Route("/helloworld/")]
         public IActionResult Index()
         {
-            string html = "<h1>Hello, World!</h1>";
+            string html = "<form method='post' action='/helloworld/welcome/'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
             return Content(html, "text/html");
         }
         // GET: /<controller>/welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")]
+        [HttpGet("welcome/{name?}")]
+        [HttpPost("welcome")]
         public IActionResult Welcome(string name = "World")
         {
             return Content($"<h1>Welcome to my app, {name}!</h1>", "text/html");

75c40459641f4bb50c3e31af3b829a37ca783226
mtkramer <themtkramer@gmail.com>
2020-11-21 20:07:34 -0500

    Reading 10.2 complete

---
+++ Exercise10 Controllers/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise10_Controllers.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello, World!</h1>";
+            return Content(html, "text/html");
+        }
+        // GET: /<controller>/welcome
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content($"<h1>Welcome to my app, {name}!</h1>", "text/html");
+        }
+
+    }
+}

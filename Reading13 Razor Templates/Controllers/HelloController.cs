8a8f88000d7e0108bdd52f2cbbc10c9102e73b49
mtkramer <themtkramer@gmail.com>
2020-11-28 12:35:23 -0500

    Reading13 init

---
+++ Reading13 Razor Templates/Controllers/HelloController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reading13_Razor_Templates.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/hello'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
---
+++ Reading13 Razor Templates/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reading13_Razor_Templates.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

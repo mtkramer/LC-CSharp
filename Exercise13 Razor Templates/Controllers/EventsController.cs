347b3c9b8271d1ba0f9c557416df1a9482eb1258
mtkramer <themtkramer@gmail.com>
2020-11-28 13:44:49 -0500

    Exercise13 init

---
+++ Exercise13 Razor Templates/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise13_Razor_Templates.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.AddRange(new List<string>() { "String Loop", "Grace Hopper", "Code With Pride" });
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
---
+++ Exercise13 Razor Templates/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exercise13_Razor_Templates.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

eac312f7cacad03b4c3698b673c16772e7e506bb
mtkramer <themtkramer@gmail.com>
2020-12-03 13:17:37 -0500

    Studio14 init

---
+++ Studio14 Models/Controllers/SpaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Studio14_Models.Models;
+
+namespace Studio14_Models.Controllers
+{
+    public class SpaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/spa")]
+        public IActionResult Menu(string skintype, string manipedi)
+        {
+            Client newClient = new Client(skintype, manipedi);
+            newClient.SetFacials(newClient.SkinType);
+            ViewBag.client = newClient;
+            return View();
+        }
+    }
+}

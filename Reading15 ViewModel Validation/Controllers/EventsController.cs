861c53d9051da9610e501887f29ce8059750ac84
mtkramer <themtkramer@gmail.com>
2020-12-05 12:19:09 -0500

    Reading15 starter

---
+++ Reading15 ViewModel Validation/Controllers/EventsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reading15_ViewModel_Validation.Models;
+using Reading15_ViewModel_Validation.Data;
+
+namespace Reading15_ViewModel_Validation.Controllers
+{
+    public class EventsController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Events/Add")]
+        public IActionResult NewEvent(Event newEvent)
+        {
+            EventData.Add(newEvent);
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            //ViewBag.title = "Delete Events";
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+    }
+}

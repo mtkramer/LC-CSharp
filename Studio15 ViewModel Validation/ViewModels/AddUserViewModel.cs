e5cdcce6ee51796fca16da639ea2531c1ac94b0a
mtkramer <themtkramer@gmail.com>
2020-12-07 15:22:32 -0500

    Studio15.5 complete

--- Studio15 ViewModel Validation/ViewModels/AddUserViewModel.cs
+++ Studio15 ViewModel Validation/ViewModels/AddUserViewModel.cs
@@ -6,17 +6,17 @@ namespace Studio15_ViewModel_Validation.ViewModels
     {
         [Required(ErrorMessage = "A username is required.")]
         [StringLength(15, MinimumLength = 5, ErrorMessage = "Valid usernames must be between 5 and 15 characters.")]
-        public string Username { get; set; }
+        public string Username { get; set; } = "";
 
         [EmailAddress]
-        public string Email { get; set; }
-        
+        public string Email { get; set; } = "";
+
         [Required(ErrorMessage = "A password is required.")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Valid passwords must be between 6 and 20 characters.")]
-        public string Password { get; set; }
-        
+        public string Password { get; set; } = "";
+
         [Required(ErrorMessage = "Password verification is required.")]
-        public string VerifyPassword { get; set; }
+        public string VerifyPassword { get; set; } = "";
 
     }
 }

3cc55194a6ce37892eb056b57d5bd00b60585ce0
mtkramer <themtkramer@gmail.com>
2020-12-08 16:39:06 -0500

    Reading19 setup

---
+++ Reading19 ORM/Controllers/EventsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Reading19_ORM.Data;
+using Reading19_ORM.Models;
+using Reading19_ORM.ViewModels;
+
+namespace Reading19_ORM.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Event> events = new List<Event>(EventData.GetAll());
+
+            return View(events);
+        }
+
+        public IActionResult Add()
+        {
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+
+            return View(addEventViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                    Type = addEventViewModel.Type
+                };
+
+                EventData.Add(newEvent);
+
+                return Redirect("/Events");
+            }
+
+            return View(addEventViewModel);
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+    }
+}
---
+++ Reading19 ORM/Models/EventType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Reading19_ORM.Models
+{
+    public enum EventType
+    {
+        Conference,
+        Meetup,
+        Workshop,
+        Social
+    }
+}

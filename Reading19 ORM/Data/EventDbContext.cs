37692d6a8c1f9ab5e789022093469b13bdfd0bf8
mtkramer <themtkramer@gmail.com>
2020-12-14 15:03:02 -0500

    Added Db extension class.

---
+++ Reading19 ORM/Data/EventDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reading19_ORM.Models;
+
+namespace Reading19_ORM.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) { }
+
+    }
+}

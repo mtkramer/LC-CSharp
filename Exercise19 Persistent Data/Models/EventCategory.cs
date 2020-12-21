fe2d118cff22b3ab5b40f62bb37de49f0d14e817
mtkramer <themtkramer@gmail.com>
2020-12-21 14:19:07 -0500

    Exercise19 19.4.1 complete

---
+++ Exercise19 Persistent Data/Models/EventCategory.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Exercise19_Persistent_Data.Models
+{
+    public class EventCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public EventCategory() { }
+        public EventCategory(string name) { Name = name; }
+    }
+}

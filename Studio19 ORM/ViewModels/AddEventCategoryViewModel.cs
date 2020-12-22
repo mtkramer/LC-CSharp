417e653703b76de4c9042bfee9cd7cbe427780c3
mtkramer <themtkramer@gmail.com>
2020-12-22 15:22:30 -0500

    Studio19.5.1 complete

---
+++ Studio19 ORM/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Studio19_ORM.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 20 characters")]
+        public string Name { get; set; }
+    }
+}

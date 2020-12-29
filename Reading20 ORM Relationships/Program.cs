7ed0b2d953f0f1961b6ddf099bb513189a3679d3
mtkramer <themtkramer@gmail.com>
2020-12-29 16:48:46 -0500

    Reading20.2.2 complete

--- Reading20 ORM Relationships/Program.cs
+++ Reading20 ORM Relationships/Program.cs
@@ -8,8 +8,12 @@ namespace Reading20_ORM_Relationships
         static void Main(string[] args)
         {
             int[] nums = { 1, 2, 3, 4 };
+            // map function called .Select()
             var doubleNums = nums.Select(x => 2 * x);
             Console.WriteLine(string.Join(" ", doubleNums));
+            // filter function called .Where()
+            var evenNums = nums.Where(x => (x % 2 == 0));
+            Console.WriteLine(string.Join(" ", evenNums));
         }
     }
 }

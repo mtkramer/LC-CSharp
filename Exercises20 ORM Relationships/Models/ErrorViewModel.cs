111406fbbff59740f24d0378604beabe27371848
mtkramer <themtkramer@gmail.com>
2020-12-30 15:45:01 -0500

    Exercise20 init

---
+++ Exercises20 ORM Relationships/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exercises20_ORM_Relationships.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

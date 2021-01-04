f2ac12915677fbf55eed8fcfe332a3402a0eea4a
mtkramer <themtkramer@gmail.com>
2021-01-04 15:09:46 -0500

    Exercise20 +-EventCategory.cs for relationship to Event

--- Exercise20 ORM Relationships/Models/EventCategory.cs
+++ Exercise20 ORM Relationships/Models/EventCategory.cs
@@ -1,9 +1,12 @@
-﻿namespace Exercise20_ORM_Relationships.Models
+﻿using System.Collections.Generic;
+
+namespace Exercise20_ORM_Relationships.Models
 {
     public class EventCategory
     {
         public string Name { get; set; }
         public int Id { get; set; }
+        public List<Event> Events { get; set; }
 
         public EventCategory() { }
         public EventCategory(string name) { Name = name; }

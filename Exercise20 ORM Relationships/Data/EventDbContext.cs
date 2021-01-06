33f92bb44bcbf09bfe0c53a0d444ee37e8e65490
mtkramer <themtkramer@gmail.com>
2021-01-05 21:43:15 -0500

    Exercise20 -+EventDbContext.cs for Tags

--- Exercise20 ORM Relationships/Data/EventDbContext.cs
+++ Exercise20 ORM Relationships/Data/EventDbContext.cs
@@ -7,6 +7,7 @@ namespace Exercise20_ORM_Relationships.Data
     {
         public DbSet<Event> Events { get; set; }
         public DbSet<EventCategory> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) {  }
     }
--- Exercise20 ORM Relationships/Models/Tag.cs
+++ Exercise20 ORM Relationships/Models/Tag.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Exercise20_ORM_Relationships.Models
+﻿namespace Exercise20_ORM_Relationships.Models
 {
     public class Tag
     {

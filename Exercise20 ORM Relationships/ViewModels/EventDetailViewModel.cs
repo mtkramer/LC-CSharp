f660427b5d93abb33166de02fa1fc9598fe902c2
mtkramer <themtkramer@gmail.com>
2021-01-06 18:08:03 -0500

    Exercise20 ++EventDetailViewModel

---
+++ Exercise20 ORM Relationships/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Exercise20_ORM_Relationships.Models;
+
+namespace Exercise20_ORM_Relationships.ViewModels
+{
+    public class EventDetailViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+        public string CategoryName { get; set; }
+
+        public EventDetailViewModel (Event evt)
+        {
+            Name = evt.Name;
+            Description = evt.Description;
+            ContactEmail = evt.ContactEmail;
+            CategoryName = evt.Category.Name;
+        }
+        public EventDetailViewModel () { }
+    }
+}

b7977e36f7a3e3d3e52d649a3358b1228d28da35
mtkramer <themtkramer@gmail.com>
2021-01-11 15:06:49 -0500

    Exercise20 -+EventDbContext.cs for EventTags and setup of their join table

--- Exercise20 ORM Relationships/Data/EventDbContext.cs
+++ Exercise20 ORM Relationships/Data/EventDbContext.cs
@@ -8,7 +8,14 @@ namespace Exercise20_ORM_Relationships.Data
         public DbSet<Event> Events { get; set; }
         public DbSet<EventCategory> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<EventTag> EventTags { get; set; }
 
         public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) {  }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<EventTag>().HasKey(et => new { et.EventId, et.TagId });
+        }
+
     }
 }

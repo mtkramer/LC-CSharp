6bd9081ef2c9f9f7e7ce0cf5042ea7dac4605289
mtkramer <themtkramer@gmail.com>
2021-01-12 22:24:18 -0500

    Exercise20 -+EventDetailViewModel.cs for displaying EventTag

--- Exercise20 ORM Relationships/ViewModels/EventDetailViewModel.cs
+++ Exercise20 ORM Relationships/ViewModels/EventDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Exercise20_ORM_Relationships.Models;
+using System.Collections.Generic;
 
 namespace Exercise20_ORM_Relationships.ViewModels
 {
@@ -8,13 +9,23 @@ namespace Exercise20_ORM_Relationships.ViewModels
         public string Description { get; set; }
         public string ContactEmail { get; set; }
         public string CategoryName { get; set; }
+        public string TagText { get; set; }
 
-        public EventDetailViewModel (Event evt)
+        public EventDetailViewModel (Event evt, List<EventTag> eventTags)
         {
             Name = evt.Name;
             Description = evt.Description;
             ContactEmail = evt.ContactEmail;
             CategoryName = evt.Category.Name;
+            TagText = "";
+            for(int i = 0; i < eventTags.Count; i++)
+            {
+                TagText += "#" + eventTags[i].Tag.Name;
+                if(i < eventTags.Count)
+                {
+                    TagText += ", ";
+                }
+            }
         }
         public EventDetailViewModel () { }
     }
